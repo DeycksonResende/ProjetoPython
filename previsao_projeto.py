de7ffd09d3cac508bd2bd6e04323c5c0ce11438f
unknown <williamkhayashi@gmail.com>
2024-04-23 11:49:41 -0300

    commit da mudanca de api

--- previsao_projeto.py
+++ previsao_projeto.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request
 import requests
 
+
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
     cidade = request.form['name']
-    api_key = "8b08d97d1c73496296b98abf0ba86b82" # Mover para variáveis de ambiente
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}"
+    with open('api_keys.txt', 'r') as f:
+        key = f.read()
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={key}"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
